ab9574732bd222ba10938d701b0c1b3a744a11c9
Kimoto-Norihiro <n000r111code@gmail.com>
2023-05-20 14:24:42 +0900

    use env

--- main.go
+++ main.go
@@ -3,11 +3,11 @@ package main
 import (
 	"fmt"
 	"log"
-	"net/http"
+	// "net/http"
+	"os"
 
 	"github.com/gin-gonic/gin"
 	"github.com/line/line-bot-sdk-go/v7/linebot"
-
 	// "gorm.io/driver/mysql"
 	// "gorm.io/gorm"
 )
@@ -19,13 +19,11 @@ type User struct {
 
 func main() {
   router := gin.Default()
-  client := &http.Client{}
 
   // line-bot
   bot, _ := linebot.New(
-    "1f0cc8bdd3631e80428d94f001061e78",
-    "8zuf8b9zJkAQVX+oM7jK2aY095e7G7E7CYJE2WthY6+bRhphEv++j6lwaZO4/psmlIImaqXg0tnvIzUdT+GOA995UAbOhCyycTZJCCucx11Iar6fpdSp9bMr8whf/nm55bYQJGchDU4Iv3Z8bIDT4AdB04t89/1O/w1cDnyilFU=",
-    linebot.WithHTTPClient(client),
+    os.Getenv("CHANNEL_SECRET"),
+    os.Getenv("CHANNEL_ACCESS_TOKEN"),
   )
 
   // db
@@ -40,7 +38,6 @@ func main() {
   // user := User{Name: "Genie"}
   // db.Create(&user)
 
-  // line-bot
   router.GET("/bot", func(c *gin.Context) {
     events, err := bot.ParseRequest(c.Request)
     if err != nil {
@@ -78,6 +75,6 @@ func main() {
   //   c.JSON(200, user)
   // })
 
-  fmt.Println("Server running on port 8080")
-  router.Run(":8080")
+  fmt.Println("Server running on port "+os.Getenv("PORT"))
+  router.Run(":"+os.Getenv("PORT"))
 }
